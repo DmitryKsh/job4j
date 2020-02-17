3f5230598ece8a8cd324ec0635a3e3676c0552cf
DmitryKrs <dcphosphene@gmail.com>
2020-02-17 21:50:42 +0300

    4.5. Отладка программы в IDEA[#233420]

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -2,19 +2,19 @@ package ru.job4j.condition;
 
 public class SqMax {
     public static int max(int first, int second, int third, int forth) {
-        int result = first;
-        if (first < second) {        // testing: Было (first > second) изменил на (first < second)
-            if (first < third) {
+        int result = forth;
+        if (first > second) {
+            if (first < third) {        //max7: изменил first > third на first < third
                 if (first > forth) {
-                    result = forth;
+                    result = third;
                 }
             }
-        } else if (second > third) {    // testing: Было (second < third) изменил на (second > third)
-            if (second > forth) {      // testing: Было (second < forth) изменил на (second > forth)
+        } else if (second < third) {    //max6: изменил second > third на second < third
+            if (second < forth) {       //max6: изменил second > forth на second < forth
                 result = first;
             }
-        } else if (third > forth) {     // testing: Было (third > forth) изменил на (third < forth)
-            result = forth;
+        } else if (third > forth) {
+            result = second;
         }
         return result;
     }
--- chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -6,14 +6,20 @@ import static org.junit.Assert.assertThat;
 
 public class SqMaxTest {
     @Test
-    public void max () {
+    public void max2 () {
         int result = SqMax.max(6,4,7,5);
-        assertThat(result, is(5));
+        assertThat(result, is(4));
     }
 
     @Test
     public void testing() {
         int result = SqMax.max(1,9,3,8);
         assertThat(result, is(1));
+    }
+
+    @Test
+    public void max6 () {
+        int result = SqMax.max(6,4,7,5);
+        assertThat(result, is(6));
     }
 }
