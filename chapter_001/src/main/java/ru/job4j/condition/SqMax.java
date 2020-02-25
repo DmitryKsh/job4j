13aee529d437a05a493427a3d3b67cb3b6d8bba8
DmitryKrs <dcphosphene@gmail.com>
2020-02-25 18:12:38 +0300

    4.5. Отладка программы в IDEA[#233420]

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -2,24 +2,21 @@ package ru.job4j.condition;
 
 public class SqMax {
     public static int max(int first, int second, int third, int forth) {
-        int result = first;
-        if (first > second) { // изменил
-            if (first < third) {
-                if (first > forth) { // изменил
-                    result = forth; // изменил.
+        int result = forth;
+        if (first > second) {
+            if (first > third) {
+                if (first > forth) {
+                    result = third;
                 }
             }
-        } else if (second > third) {
-            if (second > forth) {
+        } else if (second < third) {
+            if (second < forth) {
                 result = first;
             }
-        } else if (third > forth) {
+        } else if (third < forth) {
             result = second;
-        }
-        if (second < third ) {
-            if (second < forth) {
-                result = second;
-            }
+        } else if (second < first) {
+            result = forth;
         }
         return result;
     }
--- chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -6,8 +6,26 @@ import static org.junit.Assert.assertThat;
 
 public class SqMaxTest {
     @Test
-    public void max2 () {
+    public void max7 () {
+        int result = SqMax.max(6,4,7,5);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void max6 () {
+        int result = SqMax.max(6,4,7,5);
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void max4 () {
         int result = SqMax.max(6,4,7,5);
         assertThat(result, is(4));
+    }
+
+    @Test
+    public void max5 () {
+        int result = SqMax.max(6,4,7,5);
+        assertThat(result, is(5));
     }
 }
