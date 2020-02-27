d102a13e8ba231003bdf820d96ec8e09744aac5c
DmitryKrs <dcphosphene@gmail.com>
2020-02-27 16:33:10 +0300

    5.8. JavaDoc. Code convention[#233507]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,12 +1,28 @@
 package ru.job4j.calculator;
 
+/**
+ *  Calculator
+ * Класс Calculator предназначен для вычисления суммы входящих чисел.
+ * @author Dmitry Kreshchenok (dcphosphene@gmail.com)
+ */
+
 public class Calculator {
+    /**
+     * Method main.
+     * Передает значение аргументам.
+     * @param args
+     */
     public static void main (String[] args ) {
         Calculator.plus(100,500);
         Calculator.plus(4,2);
         Calculator.plus(3,5);
     }
 
+    /**
+     * Method plus.
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+/**
+ *  Package for calculator task.
+ *
+ * @author Dmitry Kreshchenok (dcphosphene@gmail.com)
+ * @version $id$
+ * @since 1.0
+ */
+package ru.job4j.calculator;
+
