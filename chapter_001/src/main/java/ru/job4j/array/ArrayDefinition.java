24e5d3f444621b676ba061c3d95263bb6f34ea60
DmitryKrs <dcphosphene@gmail.com>
2020-03-01 14:57:39 +0300

    6.0.3. Массивы и цикл for.[#233443]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,18 +2,12 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args){
-        short [] ages = new short [10];
-        String [] surnames = new String[100500];
-        float [] prices = new float[40];
-
-        String[] name = new String[3];
-
-        name[0] = "Petr Arsentev";
-        name[1] = "Edward Ford";
-        name[2] = "Patrick Stone";
-
-        System.out.println(name[0]);
-        System.out.println(name[1]);
-        System.out.println(name[2]);
+        int[] array = new int[5];
+        for (int index = 0; index < array.length; index++) {
+            array[index]= index * 2 + 3;
+        }
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
     }
 }
