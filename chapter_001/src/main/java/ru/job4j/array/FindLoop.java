0b784e28fb9060d3017c558665f7e32e06b78cad
DmitryKrs <dcphosphene@gmail.com>
2020-03-07 23:38:00 +0300

    4.5. Отладка программы в IDEA[#233420]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,14 +1,23 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public int indexOf(int[] data, int el) {
-        int rst = -1;
-        for (int index = 0; index < data.length; index++ ) {
-            if (data[index] == el) {
-                rst = index;
-                break;
+    public static int indexOf(int[] data, int value) {
+        int result = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == value) {
+                result = index; // или result = 0;
             }
         }
-        return rst;
+        return result;
+    }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int result = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (; start < finish; start++) {
+            if (data[start] == el) {
+                result = start;
+            }
+        }
+        return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -5,43 +5,24 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
+
     @Test
-    public void whenArrayHas5Then0() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
-        int value = 5;
-        int result = find.indexOf(input, value);
+    public void whenArrayHasLength5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
         int expect = 0;
         assertThat(result, is(expect));
     }
 
     @Test
-    public void whenArrayHas6Then1() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {8, 22, 13};
-        int value = 21;
-        int result = find.indexOf(input, value);
-        int expect = -1;
-        assertThat(result, is(expect));
-    }
-
-    @Test
-    public void whenArrayHas8Then1() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {18, 0, 3};
-        int value = 8;
-        int result = find.indexOf(input, value);
-        int expect = -1;
-        assertThat(result, is(expect));
-    }
-
-    @Test
-    public void whenArrayHas2Then0() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {2, 3, 4,5,6};
-        int value = 2;
-        int result = find.indexOf(input, value);
-        int expect = 0;
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, el, start, finish);
+        int expect = 3;
         assertThat(result, is(expect));
     }
 }
