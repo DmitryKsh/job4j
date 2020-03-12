7c12972013ea3b63ee09ff9f0ae4875582040b2c
DmitryKrs <dcphosphene@gmail.com>
2020-03-12 22:52:32 +0300

    6.5.1. Поиск минимального числа в массиве.[#233437]

--- chapter_001/src/main/java/ru/job4j/array/Min.java
+++ chapter_001/src/main/java/ru/job4j/array/Min.java
@@ -3,17 +3,9 @@ package ru.job4j.array;
 public class Min {
     public static int findMin(int[] array) {
         int min = array[0];
-        for ( int index = 0; index < array.length; index++) {
-            if ( min > array[index]){
-                int temp = array[index];
-                array[index] = array[0];
-                array[0] = temp;
-                min = temp;
-            }
-            if ( min < array[index] ) {
-                int temp = array[0];
-                array[0] = array[index];
-                array[index] = temp;
+        for (int index = 0; index < array.length; index++) {
+            if ( array[index] < min ) {
+                min = array[index];
             }
         }
         return min;
--- chapter_001/src/test/java/ru/job4j/array/MinTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MinTest.java
@@ -7,16 +7,16 @@ import static org.hamcrest.core.Is.is;
 public class MinTest {
     @Test
     public void whenFirstMin(){
-        assertThat(Min.findMin(new int[]{1,0,5,10}), is(0));
+        assertThat(Min.findMin(new int[]{7,0, 5, 10}), is(0));
     }
 
     @Test
     public void WhenLastMin(){
-        assertThat(Min.findMin(new int[] {10,5,4,3}), is (3));
+        assertThat(Min.findMin(new int[] {7,10, 5, 3}), is (3));
     }
 
     @Test
     public void WhenMiddleMin() {
-        assertThat(Min.findMin(new int[] {3,6,2,10}), is(2));
+        assertThat(Min.findMin(new int[] {7,10, 2, 5}), is(2));
     }
 }
